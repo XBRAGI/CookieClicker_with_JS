3a20cebbc6a8b15e2028ee14e25ff3b93ac13752
XBRAGI <kateoleynik2002@mail.ru>
2023-02-16 15:41:02 +0300

    cookie_finish

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-const cookieButton = document.getElementById(button)
+let h2Id = document.getElementById('h2')
+let buttonId = document.getElementById('button')
+let buttonReset = document.getElementById('button-reset')
+let buttonX2 = document.getElementById('button-x2')
+let buttonX4 = document.getElementById('button-x4')
+let buttonX6 = document.getElementById('button-x6')
+let counter = 0
 
-cookieButton.onclick = function() {
-	alert('Привет')
-}
+buttonId.addEventListener('click', function() {
+	counter = counter + 1
+	h2Id.textContent = 'Печенья: ' + counter
+})
+
+buttonX2.addEventListener('click', function() {
+	buttonId.addEventListener('click', function() {
+		counter = counter + 1
+		h2Id.textContent = 'Печенья: ' + counter
+	})
+})
+
+buttonX4.addEventListener('click', function() {
+	buttonId.addEventListener('click', function() {
+		counter = counter + 3
+		h2Id.textContent = 'Печенья: ' + counter
+	})
+})
+
+buttonX6.addEventListener('click', function() {
+	buttonId.addEventListener('click', function() {
+		counter = counter + 5
+		h2Id.textContent = 'Печенья: ' + counter
+	})
+})
+
+buttonReset.addEventListener('click', function() {
+	counter = 0
+	h2Id.textContent = 'Печенья: ' + counter
+})
